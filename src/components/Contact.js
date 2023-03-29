6360f4ed71172a17c08167ac8b9fcc9f25dc086e
Roberto Jan <lsbk0391@gmail.com>
2023-03-29 08:53:39 -0400

    added more styles, media queries, font awesome library

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,30 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
     return (
-        <h2>This is the Contact Page</h2>
+        <div className="contact">
+            <h2>Get in Touch</h2>
+            <ul class="contactList">
+                <li>
+                    <a href="https://www.linkedin.com/in/robertojan/">
+                    <FontAwesomeIcon icon={faLinkedin} />
+                        </a>
+                </li>
+                <li>
+                    <a href="https://github.com/RobertoJan">
+                    <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:roberto.janliao@gmail.com">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                        </a>
+                </li>
+            </ul>
+            <p>Thank you for checking this project. Please feel free to contact me through email about work/learning opportunities/feedback or let's connect on LinkedIn!</p>
+        </div>
     )
 }
 
